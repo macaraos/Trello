b884ada8e576c9306aee44f27fb628fcc0d05a3e
macaraos <macarena.araos.p@gmail.com>
2017-11-12 23:47:03 -0300

    actualizando archivos trello

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var boton = getElementById("press");
+boton.onclick = formulario;
+var button1 = document.getElementById("press");
+var button2 = document.getElementById("caja");
+
+button1.addEventListener("click", function(){
+	button1.classList.add("ocult");
+	button2.classList.remove("ocult");
+});
